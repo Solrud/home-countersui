c6de55e4c557b7aa6975a6db9e47c15cb37ea9d3
ilin-mi <ilin-mi@avid.ru>
2025-03-31 14:37:29 +0500

    Тема записывается в localStorage, прелоадинг роутинга в главную страницу, мета теги для адаптивности, новый спиннер и зацентрованный.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import {Component, OnInit} from '@angular/core';
-import {HomeCountersService} from "./data/service/HomeCounters/home-counters.service";
+import {ThemeSwitchService} from "./data/service/Theme-switch/theme-switch.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  host: {class: 'd-flex h-100 w-100 flex-column'}
+  host: {class: 'd-flex h-100 w-100 flex-column bg-main'}
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
 
+  constructor(private themeService: ThemeSwitchService) {
+  }
+
+  ngOnInit() {
+    let lsTheme = localStorage.getItem('theme');
+
+    if (lsTheme) {
+      this.themeService.changeTheme(lsTheme as 'light' | 'dark');
+    } else {
+      localStorage.setItem('theme', 'light');
+    }
+  }
 }
--- src/app/data/service/Theme-switch/theme-switch.service.ts
+++ src/app/data/service/Theme-switch/theme-switch.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
+import {Meta} from "@angular/platform-browser";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeSwitchService {
-  theme: 'light' | 'dark' = 'light';
+  private theme: 'light' | 'dark' = 'light';
 
-  constructor() { }
+  constructor(private meta: Meta) {}
+
+  public getTheme(): string{
+    return this.theme;
+  }
+
+  private updateMetaTheme(color: string): void {
+    this.meta.updateTag({name: 'theme-color', content: color})
+  }
+
+  changeTheme(theme: 'light' | 'dark'): void {
+    this.theme = theme;
+    this.theme == 'light' ? this.updateMetaTheme('#87bdfa') : this.updateMetaTheme('#244c94')
+    document.body.setAttribute('data-theme', this.theme);
+
+    localStorage.setItem('theme', this.theme);
+  }
 
   toggleTheme(): void{
     this.theme = this.theme === 'light' ? 'dark' : 'light';
-    document.body.setAttribute('data-theme', this.theme)
-  }
 
+    this.theme == 'light' ? this.updateMetaTheme('#87bdfa') : this.updateMetaTheme('#244c94')
+    document.body.setAttribute('data-theme', this.theme);
+
+    localStorage.setItem('theme', this.theme);
+  }
 }
